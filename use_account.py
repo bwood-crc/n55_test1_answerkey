f024d24fbc262f7ad13feb210446204c56c9092f
Brendan <brendanwood@live.com>
2023-09-30 13:50:26 -0400

    Added an example.

--- use_account.py
+++ use_account.py
@@ -6,5 +6,8 @@ s2 = SavingsAccount("Kids Savings")
 # The following shouldn't be possible since it's abstract.
 # a1 = Account("Generic Account 1")
 
+s1.deposit(Decimal(10.00))
+s1.withdraw(Decimal(5.00))
+
 print(s1)
 print(s2)
